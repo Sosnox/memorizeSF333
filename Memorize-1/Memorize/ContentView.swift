5acaa95ce12c9d1278b012542035118fe5ac52ea
INTHAT SRIPHAO <intspxo@gmail.com>
2023-10-30 19:12:28 +0700

    Import File Jarn

---
+++ Memorize-1/Memorize/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Class Demo on 5/10/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let emojis = ["👻", "🎃", "🕷️", "👹", "💀", "🕸️", "🧙", "🙀", "👿", "😱", "☠️", "🍭"]
+    
+    @State var cardCount = 4
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                }
+            }
+            .foregroundColor(.orange)
+            
+            HStack {
+                Button(action: {
+                    if cardCount > 1 {
+                        cardCount -= 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.badge.minus.fill")
+                })
+                Spacer()
+                Button(action: {
+                    if cardCount < emojis.count {
+                        cardCount += 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.badge.plus.fill")
+                })
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize-1/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Class Demo on 5/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
